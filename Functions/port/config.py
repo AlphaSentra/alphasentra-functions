5c0fc21a9ab4846374ee8eb07cbfd6c9d5df9fcd
Daiviet HUYNH <daiviet.huynh@gmail.com>
2026-08-06 13:56:33 +0700

    simplify-caching-normalise-24h

--- Functions/port/config.py
+++ Functions/port/config.py
@@ -23,10 +23,10 @@ OPT_LONG_SHORT = False
 OPT_GROSS_EXPOSURE = 2.0
 
 # Cache TTLs (in seconds)
-CACHE_TTL_REPORT = 20 * 60 * 60  # 20 hours
-CACHE_TTL_PRICE = 6 * 60 * 60    # 6 hours
-CACHE_TTL_SECTOR = 20 * 60 * 60  # 20 hours
-CACHE_TTL_ETORO = 6 * 60 * 60    # 6 hours
+CACHE_TTL_REPORT = 24 * 60 * 60  # 24 hours
+CACHE_TTL_PRICE = 24 * 60 * 60    # 24 hours
+CACHE_TTL_SECTOR = 24 * 60 * 60  # 24 hours
+CACHE_TTL_ETORO = 24 * 60 * 60    # 24 hours
 CACHE_TTL_ETORO_PI = 24 * 60 * 60  # 24 hours
 
 # Default market data provider
